5829e789121feb99394a43d47309228ce2b52900
Julynish <julynish@mail.ru>
2022-11-13 14:18:03 +0300

    fixed setStatus

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
-import axios from 'axios';
-import React, { Component, useEffect, useRef, useState, setStatus, useCallback } from 'react';
+import React, { useEffect, useRef, useState, useCallback } from 'react';
 import './App.css';
-import { MainPage } from './component';
 
 function App() {
   const [isPaused, setIsPaused] = useState(false);
@@ -11,8 +9,6 @@ function App() {
   useEffect(() => {
     if (!isPaused) {
       ws.current = new WebSocket("ws://78.140.241.21:8300/ws/donations/?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjk5ODE5MTY4LCJqdGkiOiIxYzNlYzg5MDAyODM0NDVmOTk4MTgwMmIyODViZTk3ZiIsInVzZXJfaWQiOjF9.yVXnp1clGX9wTQuC0b6P1IrXULKa6uBte4Z6LJHYDt4"); // создаем ws соединение
-      ws.current.onopen = () => setStatus("Соединение открыто");  // callback на ивент открытия соединения
-      ws.current.onclose = () => setStatus("Соединение закрыто"); // callback на ивент закрытия соединения
 
       gettingData();
     }
@@ -35,15 +31,15 @@ function App() {
       <img src="./imgs/qr-code.svg" className='w-36 h-36' />
       {!!data &&
         <div className="flex flex-col items-center border-solid border-red-400 bg-transparent">
-            <div className="w-2/5">
-                <h1 className="text-black text-xl font-bold">{data?.author_name}</h1>
-                <p className="text-black text-sm">{data?.message}</p>
-            </div>
+          <div className="w-2/5">
+            <h1 className="text-black text-xl font-bold">{data?.author_name}</h1>
+            <p className="text-black text-sm">{data?.message}</p>
+          </div>
 
-            <div className="flex text-orange-400 text-3xl font-bold">
-                <h3> {data?.amount}</h3>
-                <p className='ml-2'> {data?.currency}</p>
-            </div>
+          <div className="flex text-orange-400 text-3xl font-bold">
+            <h3> {data?.amount}</h3>
+            <p className='ml-2'> {data?.currency}</p>
+          </div>
         </div>
       }
     </div>
