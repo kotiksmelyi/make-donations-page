8db2aca93f227565f6c84c3ecb99989a73484f57
Julynish <julynish@mail.ru>
2022-11-15 17:52:53 +0300

    add setTimeout

--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,23 @@ import React, { useEffect, useRef, useState, useCallback } from 'react';
 import './App.css';
 
 function App() {
-  const [isPaused, setIsPaused] = useState(false);
   const [data, setData] = useState(null);
   const ws = useRef();
 
   useEffect(() => {
-    if (!isPaused) {
-      ws.current = new WebSocket("ws://78.140.241.21:8300/ws/donations/?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjk5ODE5MTY4LCJqdGkiOiIxYzNlYzg5MDAyODM0NDVmOTk4MTgwMmIyODViZTk3ZiIsInVzZXJfaWQiOjF9.yVXnp1clGX9wTQuC0b6P1IrXULKa6uBte4Z6LJHYDt4"); // создаем ws соединение
-
+      ws.current = new WebSocket("ws://78.140.241.21:8300/ws/donations/?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjk5ODE5MTY4LCJqdGkiOiIxYzNlYzg5MDAyODM0NDVmOTk4MTgwMmIyODViZTk3ZiIsInVzZXJfaWQiOjF9.yVXnp1clGX9wTQuC0b6P1IrXULKa6uBte4Z6LJHYDt4"); 
       gettingData();
-    }
-
-    return () => ws.current.close(); // кода меняется isPaused - соединение закрывается
-  }, [ws, isPaused]);
+  }, [ws]);
 
   const gettingData = useCallback(() => {
-    if (!ws.current) return;
-
-    ws.current.onmessage = e => {                //подписка на получение данных по вебсокету
-      if (isPaused) return;
+    ws.current.onmessage = e => {   
       const message = JSON.parse(e.data);
       setData(message);
+      setTimeout(()=> {
+        setData(null)
+      }, 5000)
     };
-  }, [isPaused]);
+  }, []);
 
   return (
     <div className="flex flex-col items-center bg-transparent">
@@ -32,11 +26,11 @@ function App() {
       {!!data &&
         <div className="flex flex-col items-center border-solid border-red-400 bg-transparent">
           <div className="w-2/5">
-            <h1 className="text-black text-xl font-bold">{data?.author_name}</h1>
-            <p className="text-black text-sm">{data?.message}</p>
+            <h1 className="text-orange-400 text-2xl font-bold text-center">{data?.author_name}</h1>
+            <p className="text-black text-center text-xl">{data?.message}</p>
           </div>
 
-          <div className="flex text-orange-400 text-3xl font-bold">
+          <div className="flex text-orange-400 text-4xl font-bold">
             <h3> {data?.amount}</h3>
             <p className='ml-2'> {data?.currency}</p>
           </div>
